0bd83d24a7a7f50076ebebfbf9785af68fd1dcd1
FaustSJ <s_j_faust@comcast.net>
2018-11-30 22:40:18 -0500

    Finished~

---
+++ Sarah_Faust.py
@@ -0,0 +1,393 @@
+#!/usr/bin/env python3
+
+'''
+Revature is building a new API! This API contains functions for validating data, 
+solving problems, and encoding data. 
+The API consists of 10 functions that you must implement.
+Guidelines:
+1) Edit the file to match your first name and last name with the format shown.
+2) Provide tests in the main method for all functions, We should be able to run
+this script and see the outputs in an organized manner.
+3) You can leverage the operating system if needed, however, do not use any non
+legacy command that solves the problem by just calling the command.
+4) We believe in self commenting code, however, provide comments to your solutions
+and be organized.
+5) Leverage resources online if needed, but remember, be able to back your solutions
+up since you can be asked.
+6) Plagiarism is a serious issue, avoid it at all costs.
+o7) Don't import external libraries which are not python native
+8) Don't change the parameters or returns, follow the directions.
+9) Assignment is optional, but totally recommend to achieve before Monday for practice.
+Happy Scripting!
+© 2018 Revature. All rights reserved.
+'''
+
+'''
+Use the main function for testing purposes and to show me results for all functions.
+'''
+def main():
+	print("-----------------------------------------")
+	print("reverse")
+	print("before: Hello")
+	print("after: " + reverse("Hello"))
+	print("-----------------------------------------")
+	print("acronym")
+	print("Before: Oh My Gosh")
+	print("After: " + acronym("Oh My Gosh"))
+	print("Before: National Aeronautics and Space Administration")
+	print("After: " + acronym("National Aeronautics and Space Administration"))
+	print("-----------------------------------------")
+	print("whichTriangle")
+	print("Given: 2, 3, 2")
+	print("Result: " + whichTriangle(2,3,2))
+	print("Given: 5, 5, 5")
+	print("Result: " + whichTriangle(5,5,5))
+	print("Given: 5, 6, 7")
+	print("Result: " + whichTriangle(5,6,7))
+	print("-----------------------------------------")
+	print("scrabble")
+	print("word: street")
+	print("score (should be 6): " + str(scrabble("street")))
+	print("word: OxyphenButazone")
+	print("score (should be 41): " + str(scrabble("OxyphenButazone")))
+	print("-----------------------------------------")
+	print("armstrong")
+	print("Given: 5")
+	print("Result (should be True): " + str(armstrong(5)))
+	print("Given: 100")
+	print("Result (should be False): " + str(armstrong(100)))
+	print("Given: 9474")
+	print("Result (should be True): " + str(armstrong(9474))) 
+	print("-----------------------------------------")
+	print("primeFactors")
+	print("Given 901255")
+	print("Returns (should be 5, 17, 23, 461): " + str(primeFactors(901255)))
+	print("Given 12")
+	print("Returns (should be 2, 2, 3): " + str(primeFactors(12)))
+	print("-----------------------------------------")
+	print("pangram")
+	print("The quick brown fox jumps over the lazy dog")
+	print("Should be true: " + str(pangram("The quick brown fox jumps over the lazy dog")))
+	print("a quick movement of the enemy will jeopardize five gunboats") 
+	print("Should be false: " + str(pangram("a quick movement of the enemy will jeopardize five gunboats")))
+	print("-----------------------------------------")
+	print("sort")
+	print("Before: [2,4,5,1,3,1]")
+	print("After: " + str(sort([2,4,5,1,3,1])))
+	print("-----------------------------------------")
+	print("rotate")
+	print("Given 21, 'Let's eat, Grandma!'")
+	print("Result (should be 'Gzo'n zvo, Bmviyhv!'): " + rotate(21, "Let's eat, Grandma!"))
+	print("-----------------------------------------")
+	print("evenAndOdds")
+	evenAndOdds()
+
+
+'''
+1. Reverse a String. Example: reverse("example"); -> "elpmaxe"
+Rules:
+- Do NOT use built-in tools
+- Reverse it your own way
+param: str
+return: str
+'''
+def reverse(string):
+	rev = []
+	#equivalent to for((i=string.length-1); i>-1; i--)
+	for i in range((len(string)-1), -1, -1):
+		rev.append(string[i])
+	return ''.join(rev)
+		
+
+'''
+2. Convert a phrase to its acronym. Techies love their TLA (Three Letter
+Acronyms)! Help generate some jargon by writing a program that converts a
+long name like Portable Network Graphics to its acronym (PNG).
+param: str
+return: str
+'''
+def acronym(phrase):
+	split_phrase = phrase.split(" ")
+	acronym = ""
+	for word in split_phrase:
+		if word[0].isupper():
+			acronym += word[0]
+	return acronym
+	
+'''
+3. Determine if a triangle is equilateral, isosceles, or scalene. An
+equilateral triangle has all three sides the same length. An isosceles
+triangle has at least two sides the same length. (It is sometimes specified
+as having exactly two sides the same length, but for the purposes of this
+exercise we'll say at least two.) A scalene triangle has all sides of
+different lengths.
+param: float, float, float
+return: str -> 'equilateral', 'isoceles', 'scalene'
+'''
+def whichTriangle(sideOne, sideTwo, sideThree):
+	if (sideOne==sideTwo) and (sideOne==sideThree):
+		return "equilateral"
+	if (sideOne==sideTwo) or (sideOne==sideThree) or (sideTwo==sideThree):
+		return "isosceles"
+	return "scalene"
+
+'''
+4. Given a word, compute the scrabble score for that word.
+--Letter Values-- Letter Value A, E, I, O, U, L, N, R, S, T = 1; D, G = 2; B,
+C, M, P = 3; F, H, V, W, Y = 4; K = 5; J, X = 8; Q, Z = 10; Examples
+"cabbage" should be scored as worth 14 points:
+3 points for C, 1 point for A, twice 3 points for B, twice 2 points for G, 1
+point for E And to total:
+3 + 2*1 + 2*3 + 2 + 1 = 3 + 2 + 6 + 3 = 5 + 9 = 14
+param: str
+return: int
+
+'''
+def scrabble(word):
+	word = word.lower()
+	#make a map/dictionary
+	keys = "aeioulnrstdgbcmpfhvwykjxqz"
+	values = "111111111122333344444588xx"
+	maper = {}
+	values_index = 0
+	for c in keys:
+		if (c == "q") or (c == "z"):
+			maper[c] = 10
+		else:
+			maper[c] = values[values_index]
+		values_index += 1
+	#track the score
+	score = 0
+	for letter in word:
+		score += int(maper[letter])
+	return score
+
+'''
+5. An Armstrong number is a number that is the sum of its own digits each
+raised to the power of the number of digits.
+For example:
+9 is an Armstrong number, because 9 = 9^1 = 9 10 is not an Armstrong number,
+because 10 != 1^2 + 0^2 = 2 153 is an Armstrong number, because: 153 = 1^3 +
+5^3 + 3^3 = 1 + 125 + 27 = 153 154 is not an Armstrong number, because: 154
+!= 1^3 + 5^3 + 4^3 = 1 + 125 + 64 = 190 Write some code to determine whether
+a number is an Armstrong number.
+param: int
+return: bool
+
+'''
+def armstrong(number):
+	#check if it's less than ten
+	if number < 10:
+		return True
+	#otherwise, pull each digit out
+	digits = []
+	for num in str(number):
+		digits.append(num)
+	#then calculate if it's armstrong
+	total = 0
+	for digit in digits:
+		total += int(digit) ** len(digits)
+	return number==total
+
+'''
+6. Compute the prime factors of a given natural number.
+A prime number is only evenly divisible by itself and 1.
+ 
+Note that 1 is not a prime number.
+param: int
+return: list
+
+'''
+def primeFactors(number):
+	prime_factors = []
+	#check initial primes
+	if (number % 2) == 0:
+		prime_factors.append(2)
+		reduced = number/2
+		while (reduced%2)==0:
+			prime_factors.append(2)
+			reduced /= 2
+	if (number % 3) == 0:
+		prime_factors.append(3)
+		reduced = number/3
+		while (reduced%3) == 0:
+			prime_factors.append(3)
+			reduced /= 3
+	#find other potential primes
+	current_num = 5
+	while (current_num**2) <= number:
+		if (number%current_num) == 0:
+			#make sure it's not divisible by listed primes
+			to_add = True
+			for num in prime_factors:
+				if (current_num%num) == 0:
+					to_add = False
+			if to_add:
+				prime_factors.append(current_num)
+				reduced = number/current_num
+				while (reduced%current_num) == 0:
+					prime_factors.append(current_num)
+					reduced /= current_num
+		
+		if (number%(current_num+2)) == 0:
+			#make sure it's not divisible by listed primes
+			to_add = True
+			for num in prime_factors:
+				if ((current_num+2)%num) == 0:
+					to_add = False
+			if to_add:
+				prime_factors.append(current_num+2)
+				reduced = number/(current_num+2)
+				while (reduced%(current_num+2)) == 0:
+					prime_factors.append(current_num+2)
+					reduced /= (current_num+2)	
+		current_num += 6
+	return prime_factors
+
+'''
+7. Determine if a sentence is a pangram. A pangram (Greek: pan
+gramma, "every letter") is a sentence using every letter of the alphabet at
+least once. The best known English pangram is:
+The quick brown fox jumps over the lazy dog.
+ 
+The alphabet used consists of ASCII letters a to z, inclusive, and is case
+insensitive. Input will not contain non-ASCII symbols.
+ 
+param: str
+return: bool
+
+'''
+def pangram(sentence):
+	sentence = sentence.lower()
+	#build a map/dictionary with letters as keys and their count as values
+	letter_count = {}
+	for letter in "abcdefghijklmnopqrstuvwxyz":
+		letter_count[letter] = 0
+	for char in sentence:
+		if char != " ":
+			letter_count[char] = 1
+	return (0 not in letter_count.values())
+'''
+8. Sort list of integers.
+f([2,4,5,1,3,1]) = [1,1,2,3,4,5]
+Rules:
+- Do NOT sort it with .sort() or sorted(list) or any built-in tools.
+- Sort it your own way
+param: list
+return: list
+
+'''
+def sort(numbers):
+	#a simple bubble-sort
+	first = True
+	sorted = []
+	for num in numbers:
+		if first:
+			sorted.append(num)
+			first = False
+		else:
+			for i in range((len(sorted)-1), -1, -1):
+				#if at the end of the list
+				if i == len(sorted)-1:
+					if num > sorted[i]:
+						sorted.append(num)
+						break
+				#if at the beginning of the list
+				if i == 0:
+					if num < sorted[i]:
+						sorted.insert(0, num)
+						break
+				#otherwise...
+				if num >= sorted[i]:
+					sorted.insert((i+1), num)
+					break
+					
+	return sorted
+
+'''
+9. Create an implementation of the rotational cipher, also sometimes called
+the Caesar cipher.
+The Caesar cipher is a simple shift cipher that relies on transposing all the
+letters in the alphabet using an integer key between 0 and 26. Using a key of
+0 or 26 will always yield the same output due to modular arithmetic. The
+letter is shifted for as many values as the value of the key.
+The general notation for rotational ciphers is ROT + <key>. The most commonly
+used rotational cipher is ROT13.
+A ROT13 on the Latin alphabet would be as follows:
+Plain: abcdefghijklmnopqrstuvwxyz Cipher: nopqrstuvwxyzabcdefghijklm It is
+stronger than the Atbash cipher because it has 27 possible keys, and 25
+usable keys.
+Ciphertext is written out in the same formatting as the input including
+spaces and punctuation.
+Examples: ROT5 omg gives trl ROT0 c gives c ROT26 Cool gives Cool ROT13 The
+quick brown fox jumps over the lazy dog. gives Gur dhvpx oebja sbk whzcf bire
+gur ynml qbt. ROT13 Gur dhvpx oebja sbk whzcf bire gur ynml qbt. gives The
+quick brown fox jumps over the lazy dog.
+param: int, str
+return: str
+
+'''
+def rotate(key, string):
+	#there are only 26 possible rotation sequences
+	#so we shrink the key to fit within those 26
+	key = key%26
+	#if the key==0, the no rotation takes place
+	if key == 0:
+		return string
+	#otherwise, start making the cipher by claculating each letter's new index
+	alpha = "abcdefghijklmnopqrstuvwxyz"
+	#unlike java, you can't just make a list of a preset size
+	#shifted_alpha = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 0, 1, 2, 3, 4, 5]
+	shifted_alpha = [0]*26
+	for index in range(0,26):
+		new_index = index+key
+		#make sure the new_index doesn't go out of bounds
+		if new_index>25:
+			new_index -= 26
+		shifted_alpha[new_index] = alpha[index]
+	#use a dictionary as the cipher
+	cipher = {}
+	shifted_index = 0
+	for letter in alpha:
+		cipher[shifted_alpha[shifted_index]] = letter
+		shifted_index += 1
+	#encode the given string
+	encoded = []
+	for char in string:
+		if char.isalpha():
+			if char.isupper():
+				encoded.append(cipher[char.lower()].upper())
+			else:
+				encoded.append(cipher[char])
+		else:
+			encoded.append(char)
+	return "".join(encoded)
+'''
+10. Take 10 numbers as input from the user and store all the even numbers in a file called even.txt and
+the odd numbers in a file called odd.txt.
+param: none, from the keyboard
+return: nothing
+'''
+def evenAndOdds():
+	even = open("evens.txt","w+")
+	odd = open("odds.txt","w+")
+
+	nums = str(input("please enter 10 numbers, each separated by a single space: "))
+	num_list = nums.split(" ")
+	even_count = 0
+	odd_count = 0
+	for num in num_list:
+		if (int(num)%2)==0:
+			#write to evens.txt
+			even.write(num+"\n")
+			even_count += 1
+		else:
+			#write to odds.txt
+			odd.write(num+"\n")
+			odd_count += 1
+	print(str(even_count) + " evens and " + str(odd_count) + " odds were written to files.")
+	even.close()
+	odd.close()
+
+if __name__ == "__main__":
+	main()
